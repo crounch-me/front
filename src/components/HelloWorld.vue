c3eb5f9685f0c422bf874a8b01101c676801f7d0
Sehsyha <valentin.stern@zenika.com>
2018-07-05 12:05:42 +0200

    Init vue js frontend

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Welcome to the crounch application</h2>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
