272b6846318469873bb154ef6cef360d1890bef1
Sehsyha <valentin.stern@zenika.com>
2018-07-15 12:12:08 +0200

    Delete auth0 variables

--- src/auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  clientId: '1LtxaXZfB5N9MAJFHXqZPqH9rXvzI9wk',
-  domain: 'crounch.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
-  apiUrl: 'http://localhost:3001'
-}
